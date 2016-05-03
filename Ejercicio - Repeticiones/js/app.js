2c75e2d7af0fe90c96802e9f6fbfdbd2a959c7b0
Jaime Gómez <gomezv9@hotmail.com>
2016-05-03 15:39:08 -0500

    Ejercicios de ng-repeat

--- Ejercicio - Repeticiones/js/app.js
+++ Ejercicio - Repeticiones/js/app.js
@@ -4,14 +4,26 @@ var app = angular.module('universidadApp',[ ]);
 
 app.controller('listadoCtrl', ['$scope', function($scope){
 	
-		
+		$scope.listado = ["Fernando Herrera", "Jaime Gómez"];
 
+		$scope.listadoProfesores = {
+			profesores: [{
 
+				nombre: "Fernando Herrera",
+				edad: 30,
+				clase: "PEE"
+
+			},
+			{
+
+				nombre: "Jaime Gómez",
+				edad: 29,
+				clase: "ICE"
+
+			}]
+		}
 
 }]);
-
-
-
 
 
 })();
